8a769da5615940eedc7d53e745a807bf2752b9ee
Mark Moyes <mark.moyes@cbc.ca>
2015-06-10 11:05:14 -0700

    initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # koco-date-validation-rules
 Knockout Validation rules for dates
+
+## Installation
+
+```bash
+bower install koco-date-validation-rules
+```
+
+## Usage with KOCO
+
+This is a shared module that is used in many other modules. The convention is to require the handler in the `knockout-validation-rules.js` file like so:
+
+```javascript
+define([
+  ...
+  'bower_components/koco-date-validation-rules/src/date-validation-rules'
+  ...
+],
+```
+
+## Usage
+
+There are two date validation rules included.
+
+###dateFormat
+
+Checks the current value against a user-specified date format. If no format is provided, the default is 'YYYY-MM-DD HH:mm'
+
+##dateIsAfter
+
+Checks the current value against another date to ensure that the first is later. Useful for date range pickers. 
+
+```javascript
+endDate.extend({
+    dateIsAfter: self.observableContent().startDate
+});
+```
---
+++ src/date-validation-rules.js
@@ -0,0 +1,43 @@
+// Copyright (c) CBC/Radio-Canada. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+define(['knockout', 'moment'],
+    function(ko, moment) {
+        'use strict';
+
+        // Validates that a date is in a valid date format by
+        // checking against moment. Can optionally provide the date 
+        // format as a param.
+        ko.validation.rules.dateFormat = {
+            validator: function(val, otherVal) {
+                var isValid = true,
+                	dateFormat = otherVal || 'YYYY-MM-DD HH:mm';
+
+
+                if (val) {
+                    try {
+                        isValid = moment(val, dateFormat).isValid();
+                    } catch (e) {
+                        isValid = false;
+                    }
+                }
+
+                return isValid;
+            },
+            message: 'The date format is not valid.'
+        };
+
+        // Validates that a range of dates is valid by checking that
+        // the end date is after the start date.
+        ko.validation.rules.dateIsAfter = {
+            validator: function(val, otherVal) {
+                if (val === null || otherVal === null) {
+                    return true;
+                }
+
+                return moment(val).isAfter(otherVal);
+            },
+            message: 'The end date must be after the start date'
+        };
+
+    });
