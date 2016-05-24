556b0a056d801b2a358e3732b99114541df1ec47
Maxime Forget-Séguin <maxime.forget-seguin@radio-canada.ca>
2016-05-24 13:20:21 -0400

    update to koco2

--- src/date-validation-rules.js
+++ src/date-validation-rules.js
@@ -1,77 +1,76 @@
 // Copyright (c) CBC/Radio-Canada. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-define(['knockout', 'moment', 'i18next'],
-    function(ko, moment, i18n) {
-        'use strict';
-
-        // Validates that a date is in a valid date format by
-        // checking against moment. Can optionally provide the date 
-        // format as a param.
-        ko.validation.rules.dateFormat = {
-            validator: function(val, otherVal) {
-                var isValid = true,
-                	dateFormat = otherVal || 'YYYY-MM-DD HH:mm';
+import ko from 'knockout';
+import moment from 'moment';
+import i18n from 'i18next';
 
 
-                if (val) {
-                    try {
-                        isValid = moment(val, dateFormat).isValid();
-                    } catch (e) {
-                        isValid = false;
-                    }
-                }
+// Validates that a date is in a valid date format by
+// checking against moment. Can optionally provide the date 
+// format as a param.
+ko.validation.rules.dateFormat = {
+    validator: function(val, otherVal) {
+        var isValid = true,
+            dateFormat = otherVal || 'YYYY-MM-DD HH:mm';
 
-                return isValid;
-            },
-            message: i18n.t('koco-date-validation-rules.date_format_not_valid')
-        };
 
-        // Validates that a range of dates is valid by checking that
-        // the end date is after the start date.
-        ko.validation.rules.dateIsAfter = {
-            validator: function(val, otherVal) {
-                if (val === null || otherVal === null) {
-                    return true;
-                }
+        if (val) {
+            try {
+                isValid = moment(val, dateFormat).isValid();
+            } catch (e) {
+                isValid = false;
+            }
+        }
 
-                return moment(val).isAfter(otherVal);
-            },
-            message: i18n.t('koco-date-validation-rules.end_date_after_start_date')
-        };
+        return isValid;
+    },
+    message: i18n.t('koco-date-validation-rules.date_format_not_valid')
+};
 
-        // Variation on above.
-        ko.validation.rules.dateIsSameOrAfter = {
-            validator: function(val, otherVal) {
-                if (val === null || otherVal === null) {
-                    return true;
-                }
+// Validates that a range of dates is valid by checking that
+// the end date is after the start date.
+ko.validation.rules.dateIsAfter = {
+    validator: function(val, otherVal) {
+        if (val === null || otherVal === null) {
+            return true;
+        }
 
-                return moment(val).isSame(otherVal) || moment(val).isAfter(otherVal);
-            },
-            message: i18n.t('koco-date-validation-rules.end_date_after_start_date')
-        };
+        return moment(val).isAfter(otherVal);
+    },
+    message: i18n.t('koco-date-validation-rules.end_date_after_start_date')
+};
 
-        ko.validation.rules.dateIsBefore = {
-            validator: function(val, otherVal) {
-                if (val === null || otherVal === null) {
-                    return true;
-                }
+// Variation on above.
+ko.validation.rules.dateIsSameOrAfter = {
+    validator: function(val, otherVal) {
+        if (val === null || otherVal === null) {
+            return true;
+        }
 
-                return moment(val).isBefore(otherVal);
-            },
-            message: i18n.t('koco-date-validation-rules.start_date_before_end_date')
-        };
+        return moment(val).isSame(otherVal) || moment(val).isAfter(otherVal);
+    },
+    message: i18n.t('koco-date-validation-rules.end_date_after_start_date')
+};
 
-        ko.validation.rules.DateIsSame = {
-        	validator: function(val, otherVal) {
-        	    if (val === null || otherVal === null) {
-        	        return true;
-        	    }
+ko.validation.rules.dateIsBefore = {
+    validator: function(val, otherVal) {
+        if (val === null || otherVal === null) {
+            return true;
+        }
 
-        	    return moment(val).isSame(otherVal);
-        	},
-        	message: i18n.t('koco-date-validation-rules.dates_equal')
-        };
+        return moment(val).isBefore(otherVal);
+    },
+    message: i18n.t('koco-date-validation-rules.start_date_before_end_date')
+};
 
-    });
+ko.validation.rules.DateIsSame = {
+    validator: function(val, otherVal) {
+        if (val === null || otherVal === null) {
+            return true;
+        }
+
+        return moment(val).isSame(otherVal);
+    },
+    message: i18n.t('koco-date-validation-rules.dates_equal')
+};
